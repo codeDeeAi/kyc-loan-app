44613bbdba65926a25da6514746cf2a997c21326
codeDeeAi <dunsin.bada@gmail.com>
2020-08-10 15:46:41 +0100

    initial design start

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank XYZ</title>
+    <link rel="icon" href="">
+    <!-- Font-awesome -->
+    <link href="font-awesome/all.css" rel="stylesheet">
+    <!-- Material fonts offline -->
+    <link type="text/css" href="iconfont/material-icons.css" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    
+
+    <!-- Home page layout -->
+    <div class="row">
+        <div class="container-home">
+            <div class="row">
+                <div class="col s5 m2 z-depth-2 container-home-left">
+                    <!-- Navigation -->
+                    <?php require_once 'nav.php'; ?>
+                </div>
+                <div class="col s7 m10 container-home-right">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Home page layout ends -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="jQuery/jquery-3.5.1.min.js"></script>
+    <!-- font-awesome -->
+    <script type="text/javascript" src="font-awesome/all.js"></script>
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script>
+        M.AutoInit();
+    </script>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank XYZ</title>
+    <link rel="icon" href="">
+    <!-- Font-awesome -->
+    <link href="font-awesome/all.css" rel="stylesheet">
+    <!-- Material fonts offline -->
+    <link href="iconfont/material-icons.css" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navigation -->
+    <div class="container-nav row ">
+
+        <div class="container-90">
+            <!-- Navigation Profile Header -->
+            <div class="profile">
+                <div class="container-90">
+                    <!-- Profile image -->
+                    <div class="user">
+                        <img src="imgs/money.jpg" alt="" class="avatar circle" style="width: 100px; height:100px;">
+                    </div>
+                    <!-- Details -->
+                    <div class="profile-details">
+                        <h5>Lola Smith</h5>
+                        <p>Lola@xyz.com</p>
+                        <p>Bank Agent</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Navigation Content -->
+            <div class="content z-depth-0">
+                <ul class="collapsible z-depth-0">
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- JQuery -->
+    <script type="text/javascript" src="jQuery/jquery-3.5.1.min.js"></script>
+    <!-- font-awesome -->
+    <script type="text/javascript" src="font-awesome/all.js"></script>
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script>
+        M.AutoInit();
+    </script>
+</body>
+
+</html>
